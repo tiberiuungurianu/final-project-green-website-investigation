c11fa74214b6d0522ebd8b3895983a4dcf10f702
tiberiuungurianu <ungurianu43@gmail.com>
2025-02-19 21:12:50 +0000

    Both servers are working, need to be connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, Green Website!</h1>
+      <p>Welcome to your optimised eco-friendly project!</p>
+    </div>
+  );
+}
