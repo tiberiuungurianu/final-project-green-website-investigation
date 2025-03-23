0918f79b6c050f6ff88e3eb20c1dcf7d480bc0b3
tiberiuungurianu <ungurianu43@gmail.com>
2025-03-23 22:09:15 +0000

    this will be used for further changes while running on a green server from Cloudflare

--- backend/src/main/java/com/greenwebsite/backend/config/CorsConfig.java
+++ backend/src/main/java/com/greenwebsite/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://prototypeforinvestigation.co.uk")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
